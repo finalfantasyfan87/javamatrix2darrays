8e4a2bab1e5269386fe6e35c9c6114c45b88fd3d
Jen <jenster8705@gmail.com>
2025-09-12 21:56:00 -0500

    diagonals

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -8,24 +8,40 @@ class Solution {
         int cols = matrix[0].length;
         int[][] transposed = new int[cols][rows];
         // TODO: Modify the loop to transpose the matrix in reverse order
-        for (int col = cols-1, i=0; col >=0 ; col--,i++) {
-            for (int row = 0; row<rows; row++) {
-                transposed[i][row]  = matrix[row][col];
+        for (int col = cols - 1, i = 0; col >= 0; col--, i++) {
+            for (int row = 0; row < rows; row++) {
+                transposed[i][row] = matrix[row][col];
             }
         }
-
-
         return transposed;
     }
 
-    public static void main(String[] args) {
-        int[][] seatingChart = {
-                {101, 102, 103, 104},
-                {201, 202, 203, 204},
-                {301, 302, 303, 304}
-        };
+    public static boolean isToeplitzMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        for (int row = 0; row < rows - 1; row++) {
+            for (int col = 0; col < cols - 1; col++) {
+                if (matrix[row][col] != matrix[row + 1][col + 1]) {
+                    return false;
+                }
 
-        // TODO: Store the result of transformMatrix in transposedSeating and print it
-        System.out.println(Arrays.deepToString(transformMatrix(seatingChart)));
+            }
+        }
+        return true;
     }
-}
+
+
+        public static void main (String[]args){
+            int[][] seatingChart = {
+                    {101, 102, 103, 104},
+                    {201, 202, 203, 204},
+                    {301, 302, 303, 304}
+            };
+
+            int[][] matrix = {{6, 7, 8}, {4, 6, 7}, {1, 4, 6}};
+            // TODO: Store the result of transformMatrix in transposedSeating and print it
+            //  System.out.println(Arrays.deepToString(transformMatrix(seatingChart)));
+            System.out.println(isToeplitzMatrix(matrix));
+
+        }
+    }
